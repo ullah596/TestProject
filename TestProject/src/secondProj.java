9f3dec9963c6af56c1c09b745a536181d579542d
ullah596 <57869583+ullah596@users.noreply.github.com>
2019-11-17 15:50:31 -0500

    return value

---
+++ TestProject/src/secondProj.java
@@ -0,0 +1,30 @@
+@TestNG
+public class secondProj {
+	  
+	  
+	  public static void main (String[] args){
+	    
+	    System.out.println(addval(2,3)*10 + " is total sum");
+	    
+	    
+	  }
+	  
+	  
+	  public static void addValue (int x, int y){
+	    
+	   int total = x+y;
+	   System.out.println(total);
+	   }
+	   
+	   public static int addval(int x, int y){
+	     
+	   
+	    
+	   int total = x +y;
+	   int totalsum = total;
+	    
+	    return totalsum;
+	    }
+	  }
+	  
+
